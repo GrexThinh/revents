3c69054f0b473d0bb32554bf7ebe9874006e402c
Grex Thinh <vanthinhnq@gmail.com>
2024-12-15 23:43:27 +0700

    update 2nd

--- src/app/features/events/dashboard/EventList.tsx
+++ src/app/features/events/dashboard/EventList.tsx
@@ -2,12 +2,19 @@ import { AppEvent } from "../../../types/event";
 import EventListItem from "./EventListItem";
 type Props = {
   events: AppEvent[];
+  selectEvent: (event: AppEvent) => void;
+  deleteEvent: (eventId: string) => void;
 };
-export default function EventList({ events }: Props) {
+export default function EventList({ events, selectEvent, deleteEvent }: Props) {
   return (
     <>
       {events.map((event) => (
-        <EventListItem key={event.id} event={event} />
+        <EventListItem
+          key={event.id}
+          event={event}
+          selectEvent={selectEvent}
+          deleteEvent={deleteEvent}
+        />
       ))}
     </>
   );
--- src/app/features/events/dashboard/EventListItem.tsx
+++ src/app/features/events/dashboard/EventListItem.tsx
@@ -11,14 +11,24 @@ import EventListAttendee from "./EventListAttendee";
 import { AppEvent, Attendee } from "../../../types/event";
 type Props = {
   event: AppEvent;
+  selectEvent: (event: AppEvent) => void;
+  deleteEvent: (eventId: string) => void;
 };
-export default function EventListItem({ event }: Props) {
+export default function EventListItem({
+  event,
+  selectEvent,
+  deleteEvent,
+}: Props) {
   return (
     <SegmentGroup>
       <Segment>
         <ItemGroup>
           <Item>
-            <Item.Image size="tiny" circular src={event.hostPhotoURL} />
+            <Item.Image
+              size="tiny"
+              circular
+              src={event.hostPhotoURL || "/user.png"}
+            />
             <Item.Content>
               <Item.Header>{event.title}</Item.Header>
               <Item.Description>Hosted by {event.hostedBy}</Item.Description>
@@ -41,7 +51,18 @@ export default function EventListItem({ event }: Props) {
       </Segment>
       <Segment clearing>
         <span>{event.description}</span>
-        <Button color="teal" floated="right" content="View" />
+        <Button
+          color="red"
+          floated="right"
+          content="Delete"
+          onClick={() => deleteEvent(event.id)}
+        />
+        <Button
+          color="teal"
+          floated="right"
+          content="View"
+          onClick={() => selectEvent(event)}
+        />
       </Segment>
     </SegmentGroup>
   );
